ac3f1f4d4a3ad07710a8063edc0dd2c04b0fae99
Adella <adellakenn@gmail.com>
2019-06-18 18:48:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,14 +23,6 @@ class Artist
     @@artists
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   self.all.count
-  # end
-
   def add_song(song)
     @songs << song
     song.artist = self
@@ -40,7 +32,4 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 end
