e4910d34213a3b6760e5dd068b2d82a008739499
Adella <adellakenn@gmail.com>
2019-06-18 18:43:26 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable    # responsible for defining the method as a class method vs instance (include)
-  extend Findable::ClassMethod
+  # extend Findable::ClassMethod
   include Paramable
   
   attr_accessor :name
